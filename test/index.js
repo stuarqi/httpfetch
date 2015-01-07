ebaef80e2b89bc24b0f519d76a0ae1fc607e8016
zhangsq <zhangsq@outlook.com>
2015-01-08 00:28:23 +0800

    测试程序

--- test/index.js
+++ test/index.js
@@ -1,3 +1,15 @@
-/**
- * Created by zhangsq on 15/1/7.
- */
+var HttpFetch = require('../index');
+var fs = require('fs');
+
+var httpFetch = new HttpFetch();
+
+httpFetch.fetch('http://www.puzzle8.com/cross/cross.asp', function (err, data) {
+    fs.writeFile('./data/result.html', data, {encoding : 'utf8'}, function (err) {
+        if (!err) {
+            console.log(data);
+            console.log('Done');
+        }
+    });
+}, {
+    recordId : 2
+});
